2ed4409374e2d03fad3925d334f2e6f7393bafdc
Steve Syrell <ssyrell@outlook.com>
2022-10-22 03:25:58 +0000

    Switch to using inverted ticks for row key so that table is ordered by most recent entries first. Also always lower-case the sensor ID for the partition key

--- SensorData.cs
+++ SensorData.cs
@@ -22,8 +22,10 @@ namespace SteveSyrell.PurpleAirUploadApi
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             log.LogInformation($"[SensorData] Completed reading request payload - length={requestBody.Length}");
-            
+
             var data = JsonSerializer.Deserialize<SensorDataTableEntity>(requestBody);
+            data.PartitionKey = data.SensorId.ToLower();
+            data.RowKey = string.Format("{0:D19}", DateTime.MaxValue.Ticks - DateTime.UtcNow.Ticks);
             log.LogInformation("[SensorData] Deserialization complete");
 
             TableServiceClient tableServiceClient = new TableServiceClient(Environment.GetEnvironmentVariable("STORAGE_ACCOUNT_CONNECTION_STRING"));
